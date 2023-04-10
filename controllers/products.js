002a508dc032675ae3d11769ef3692a910755212
unknown <henrytesting8@gmail.com>
2023-04-10 07:31:19 +0700

    creating shop structure  l-107

--- controllers/products.js
+++ controllers/products.js
@@ -1,50 +1,24 @@
 const Product = require("../models/product");
-// const products = [];
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("add-product", {
+  res.render("admin/add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
   });
 };
 
-//--- panduan -----
-// const express = require("express")
-// const router = express.Router()
-
-// router.get('/',(req,res,next)=>{
-//     req.body
-// })
-//--------------
 exports.postAddProduct = (req, res, next) => {
-  //   products.push({ title: req.body.title });
   const product = new Product(req.body.title);
   product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
-  //   const products = adminData.products;
-  //   const products = Product.fetchAll();
-  //   res.render("shop", {
-  //     prods: products,
-  //     pageTitle: "Shop",
-  //     path: "/",
-  //     hasProducts: products.length > 0,
-  //     activeShop: true,
-  //     productCSS: true,
-  //   });
   Product.fetchAll((products) => {
-    res.render("shop", {
+    res.render("shop/product-list", {
       prods: products,
       pageTitle: "Shop",
       path: "/",
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true,
     });
   });
 };
